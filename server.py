2d6b5615d0498061068ff09d2e1858bd55d92942
Dave Lilly <dave@lillys.org>
2024-12-17 13:45:08 -0600

    foo

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import os
 import threading
 import math
 import colorsys
+import requests
+
 from time import sleep
 from datetime import datetime
 from gpiozero import CPUTemperature
@@ -15,6 +17,12 @@ from flask_cors import CORS
 from random import randint
 from jsmin import jsmin
 from gpiozero import Button
+
+haURL = "http://homeassistant.local:8123/services/script"
+haHeaders = {
+    "Authorization": "",
+    "content-type": "application/json"
+}
 
 blinkThread = None
 crntColors = None
@@ -34,12 +42,14 @@ unicorn = UnicornWrapper()
 # get the width and height of the hardware and set it to portrait if its not
 width, height = unicorn.getShape()
 
+
 class MyFlaskApp(Flask):
-	def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
-		if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
-			with self.app_context():
-				startupRainbow()
-		super(MyFlaskApp, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
+    def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
+        if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
+            with self.app_context():
+                startupRainbow()
+        super(MyFlaskApp, self).run(host=host, port=port,
+                                    debug=debug, load_dotenv=load_dotenv, **options)
 
 
 app = MyFlaskApp(__name__, static_folder='frontend/build', static_url_path='/')
@@ -47,124 +57,128 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 def setColor(r, g, b, brightness=0.5, speed=None):
-	global crntColors
-	setPixels(r, g, b, brightness)
-	unicorn.show()
+    global crntColors
+    setPixels(r, g, b, brightness)
+    unicorn.show()
 
-	if speed is not None and speed != '':
-		crntT = threading.currentThread()
-		unicorn.clear()
-		while getattr(crntT, "do_run", True):
-			setPixels(r, g, b, brightness)
-			unicorn.show()
-			sleep(speed)
-			unicorn.clear()
-			unicorn.show()
-			sleep(speed)
+    if speed is not None and speed != '':
+        crntT = threading.currentThread()
+        unicorn.clear()
+        while getattr(crntT, "do_run", True):
+            setPixels(r, g, b, brightness)
+            unicorn.show()
+            sleep(speed)
+            unicorn.clear()
+            unicorn.show()
+            sleep(speed)
 
 
 def setPixels(r, g, b, brightness=0.5):
-	global globalBrightness, globalBlue, globalGreen, globalRed
+    global globalBrightness, globalBlue, globalGreen, globalRed
 
-	globalRed = r
-	globalGreen = g
-	globalBlue = b
+    globalRed = r
+    globalGreen = g
+    globalBlue = b
 
-	if brightness is not None:
-		globalBrightness = brightness
-		unicorn.setBrightness(brightness)
+    if brightness is not None:
+        globalBrightness = brightness
+        unicorn.setBrightness(brightness)
 
-	unicorn.setColour(r, g, b)
+    unicorn.setColour(r, g, b)
 
 
 def switchOn():
-	rgb = unicorn.hsvIntToRGB(randint(0, 360), 100, 100)
-	blinkThread = threading.Thread(target=setColor, args=(rgb[0], rgb[1], rgb[2]))
-	blinkThread.do_run = True
-	blinkThread.start()
+    rgb = unicorn.hsvIntToRGB(randint(0, 360), 100, 100)
+    blinkThread = threading.Thread(
+        target=setColor, args=(rgb[0], rgb[1], rgb[2]))
+    blinkThread.do_run = True
+    blinkThread.start()
 
 
 def switchOff():
-	global blinkThread, globalBlue, globalGreen, globalRed
-	globalRed = 0
-	globalGreen = 0
-	globalBlue = 0
-	if blinkThread is not None:
-		blinkThread.do_run = False
-	unicorn.clear()
-	unicorn.off()
+    global blinkThread, globalBlue, globalGreen, globalRed
+    globalRed = 0
+    globalGreen = 0
+    globalBlue = 0
+    if blinkThread is not None:
+        blinkThread.do_run = False
+    unicorn.clear()
+    unicorn.off()
 
 
 def halfBlink():
-	unicorn.show()
-	sleep(0.8)
-	unicorn.clear()
-	unicorn.show()
-	sleep(0.2)
+    unicorn.show()
+    sleep(0.8)
+    unicorn.clear()
+    unicorn.show()
+    sleep(0.2)
 
 
 def countDown(time):
-	showTime = time - 12
-	brightness = 0.5
-	while showTime > 0:
-		b = brightness
-		for x in range(4):
-			b = b - x
-			setPixels(255, 255, 0, b)
-			unicorn.show()
-			sleep(0.5)
-			unicorn.clear()
-		unicorn.show()
-		sleep(2)
-		showTime = showTime - 2
-	for i in range(10):
-		setPixels(255, 0, 0, 0.5)
-		halfBlink()
-	setColor(255, 0, 0, 0.5)
-	halfBlink()
-	unicorn.clear()
-	unicorn.off()
+    showTime = time - 12
+    brightness = 0.5
+    while showTime > 0:
+        b = brightness
+        for x in range(4):
+            b = b - x
+            setPixels(255, 255, 0, b)
+            unicorn.show()
+            sleep(0.5)
+            unicorn.clear()
+        unicorn.show()
+        sleep(2)
+        showTime = showTime - 2
+    for i in range(10):
+        setPixels(255, 0, 0, 0.5)
+        halfBlink()
+    setColor(255, 0, 0, 0.5)
+    halfBlink()
+    unicorn.clear()
+    unicorn.off()
 
 
 def displayRainbow(brightness, speed, run=None):
-	global crntColors
-	if speed is None:
-		speed = 0.01
-	if brightness is None:
-		brightness = 0.5
-	crntT = threading.currentThread()
-	i = 0.0
-	offset = 30
-	while getattr(crntT, "do_run", True):
-		i = i + 0.3
-		unicorn.setBrightness(brightness)
-		for x in range(0, width):
-			for y in range(0, height):
-				r = 0  # x * 32
-				g = 0  # y * 32
-				xy = x + y / 4
-				r = (math.cos((x + i) / 2.0) + math.cos((y + i) / 2.0)) * 64.0 + 128.0
-				g = (math.sin((x + i) / 1.5) + math.sin((y + i) / 2.0)) * 64.0 + 128.0
-				b = (math.sin((x + i) / 2.0) + math.cos((y + i) / 1.5)) * 64.0 + 128.0
-				r = max(0, min(255, r + offset))
-				g = max(0, min(255, g + offset))
-				b = max(0, min(255, b + offset))
-				unicorn.setPixel(x, y, int(r), int(g), int(b))
+    global crntColors
+    if speed is None:
+        speed = 0.01
+    if brightness is None:
+        brightness = 0.5
+    crntT = threading.currentThread()
+    i = 0.0
+    offset = 30
+    while getattr(crntT, "do_run", True):
+        i = i + 0.3
+        unicorn.setBrightness(brightness)
+        for x in range(0, width):
+            for y in range(0, height):
+                r = 0  # x * 32
+                g = 0  # y * 32
+                xy = x + y / 4
+                r = (math.cos((x + i) / 2.0) +
+                     math.cos((y + i) / 2.0)) * 64.0 + 128.0
+                g = (math.sin((x + i) / 1.5) +
+                     math.sin((y + i) / 2.0)) * 64.0 + 128.0
+                b = (math.sin((x + i) / 2.0) +
+                     math.cos((y + i) / 1.5)) * 64.0 + 128.0
+                r = max(0, min(255, r + offset))
+                g = max(0, min(255, g + offset))
+                b = max(0, min(255, b + offset))
+                unicorn.setPixel(x, y, int(r), int(g), int(b))
 
-		unicorn.show()
-		sleep(speed)
+        unicorn.show()
+        sleep(speed)
 
 
 def setTimestamp():
-	global globalLastCalled
-	globalLastCalled = datetime.now()
+    global globalLastCalled
+    globalLastCalled = datetime.now()
 
 
 # API Initialization
 @app.route('/', methods=['GET'])
 def root():
-	print(app.static_folder)
-	return send_from_directory(app.static_folder, 'index.html')
+    print(app.static_folder)
+    return send_from_directory(app.static_folder, 'index.html')
 
 
 @app.route('/api/on', methods=['GET', 'POST'])
@@ -195,36 +209,38 @@ def apiOff():
 
 @app.route('/api/switch', methods=['POST'])
 def apiSwitch():
-	global blinkThread, globalStatusOverwrite, globalStatus, globalLastCalledApi
+    global blinkThread, globalStatusOverwrite, globalStatus, globalLastCalledApi
 
-	if globalStatusOverwrite:
-		return make_response(jsonify({}))
+    if globalStatusOverwrite:
+        return make_response(jsonify({}))
 
-	globalLastCalledApi = '/api/switch'
-	switchOff()
-	content = json.loads(jsmin(request.get_data(as_text=True)))
-	red = content.get('red', None)
-	green = content.get('green', None)
-	blue = content.get('blue', None)
-	if red is None or green is None or blue is None:
-		return make_response(jsonify({'error': 'red, green and blue must be present and can\' be empty'}), 500)
+    globalLastCalledApi = '/api/switch'
+    switchOff()
+    content = json.loads(jsmin(request.get_data(as_text=True)))
+    red = content.get('red', None)
+    green = content.get('green', None)
+    blue = content.get('blue', None)
+    if red is None or green is None or blue is None:
+        return make_response(jsonify({'error': 'red, green and blue must be present and can\' be empty'}), 500)
 
-	if red == 0 and green == 144 and blue == 0:
-		globalStatus = 'Available'
-	elif red == 255 and green == 191 and blue == 0:
-		globalStatus = 'Away'
-	elif red == 179 and green == 0 and blue == 0:
-		globalStatus = 'Busy'
-	else:
-		globalStatus = None
+    if red == 0 and green == 144 and blue == 0:
+        globalStatus = 'Available'
+    elif red == 255 and green == 191 and blue == 0:
+        globalStatus = 'Away'
+    elif red == 179 and green == 0 and blue == 0:
+        globalStatus = 'Busy'
+    else:
+        globalStatus = None
 
-	brightness = content.get('brightness', None)
-	speed = content.get('speed', None)
-	blinkThread = threading.Thread(target=setColor, args=(red, green, blue, brightness, speed))
-	blinkThread.do_run = True
-	blinkThread.start()
-	setTimestamp()
-	return make_response(jsonify())
+    brightness = content.get('brightness', None)
+    speed = content.get('speed', None)
+    blinkThread = threading.Thread(
+        target=setColor, args=(red, green, blue, brightness, speed))
+    blinkThread.do_run = True
+    blinkThread.start()
+    setTimestamp()
+    return make_response(jsonify())
+
 
 @app.route('/api/available', methods=['GET', 'POST'])
 def availableCall():
@@ -238,7 +254,11 @@ def availableCall():
         blinkThread.do_run = True
         blinkThread.start()
         setTimestamp()
+
+        response = requests.post(f'{haURL}/office_light_available', headers = haHeaders)
+
         return make_response(jsonify())
+
 
 @app.route('/api/busy', methods=['GET', 'POST'])
 def busyCall():
@@ -252,7 +272,11 @@ def busyCall():
         blinkThread.do_run = True
         blinkThread.start()
         setTimestamp()
+
+        response = requests.post(f'{haURL}/office_light_busy', headers = haHeaders)
+
         return make_response(jsonify())
+
 
 @app.route('/api/away', methods=['GET', 'POST'])
 def awayCall():
@@ -266,92 +290,102 @@ def awayCall():
         blinkThread.do_run = True
         blinkThread.start()
         setTimestamp()
+
+        response = requests.post(f'{haURL}/office_light_away', headers = haHeaders)
+
         return make_response(jsonify())
+
 
 @app.route('/api/reset', methods=['GET', 'POST'])
 def resetCall():
-	global globalStatusOverwrite, globalStatus, globalLastCalledApi, blinkThread
-	globalStatusOverwrite = False
-	return make_response(jsonify())
+    global globalStatusOverwrite, globalStatus, globalLastCalledApi, blinkThread
+    globalStatusOverwrite = False
+    return make_response(jsonify())
 
 
 @app.route('/api/rainbow', methods=['POST'])
 def apiDisplayRainbow():
-	global blinkThread, globalStatus, globalLastCalledApi
-	globalStatus = 'rainbow'
-	globalLastCalledApi = '/api/rainbow'
-	switchOff()
-	data = request.get_data(as_text=True)
-	content = json.loads(jsmin(request.get_data(as_text=True)))
-	brightness = content.get('brightness', None)
-	speed = content.get('speed', None)
-	blinkThread = threading.Thread(target=displayRainbow, args=(brightness, speed, None))
-	blinkThread.do_run = True
-	blinkThread.start()
-	setTimestamp()
-	return make_response(jsonify())
+    global blinkThread, globalStatus, globalLastCalledApi
+    globalStatus = 'rainbow'
+    globalLastCalledApi = '/api/rainbow'
+    switchOff()
+    data = request.get_data(as_text=True)
+    content = json.loads(jsmin(request.get_data(as_text=True)))
+    brightness = content.get('brightness', None)
+    speed = content.get('speed', None)
+    blinkThread = threading.Thread(
+        target=displayRainbow, args=(brightness, speed, None))
+    blinkThread.do_run = True
+    blinkThread.start()
+    setTimestamp()
+    return make_response(jsonify())
 
 
 @app.route('/api/status', methods=['GET'])
 def apiStatus():
-	global globalStatusOverwrite, globalStatus, globalBlue, globalGreen, globalRed, globalBrightness, \
-		globalLastCalled, globalLastCalledApi, width, height, unicorn
+    global globalStatusOverwrite, globalStatus, globalBlue, globalGreen, globalRed, globalBrightness, \
+        globalLastCalled, globalLastCalledApi, width, height, unicorn
 
-	cpu = CPUTemperature()
-	return jsonify({
-		'red': globalRed, 
-		'green': globalGreen,
-		'blue': globalBlue,
-		'brightness': globalBrightness,
-		'lastCalled': globalLastCalled,
-		'cpuTemp': cpu.temperature,
-		'lastCalledApi': globalLastCalledApi, 
-		'height': height,
-		'width': width,
-		'unicorn': unicorn.getType(),
-		'status': globalStatus,
-		'statusOverwritten': globalStatusOverwrite
-	})
+    cpu = CPUTemperature()
+    return jsonify({
+        'red': globalRed,
+        'green': globalGreen,
+        'blue': globalBlue,
+        'brightness': globalBrightness,
+        'lastCalled': globalLastCalled,
+        'cpuTemp': cpu.temperature,
+        'lastCalledApi': globalLastCalledApi,
+        'height': height,
+        'width': width,
+        'unicorn': unicorn.getType(),
+        'status': globalStatus,
+        'statusOverwritten': globalStatusOverwrite
+    })
+
 
 @app.route('/api/rotate', methods=['GET'])
 def apiRotate():
-        global globalStatus
-        if globalStatus == 'Busy':
-          awayCall()
-        elif globalStatus == 'Away':
-          availableCall()
-        elif globalStatus == 'Available':
-          apiOff()
-        else:
-          busyCall()
+    global globalStatus
+    if globalStatus == 'Busy':
+        awayCall()
+    elif globalStatus == 'Away':
+        availableCall()
+    elif globalStatus == 'Available':
+        apiOff()
+    else:
+        busyCall()
 
-        return make_response(jsonify({'status': globalStatus}), 200)
+    return make_response(jsonify({'status': globalStatus}), 200)
+
 
 @app.route('/api/togglemute', methods=['GET'])
 def toggleMute():
-  global globalStatus
-  if globalStatus == 'Busy':
-    awayCall()
-  else:
-    busyCall()
+    global globalStatus
+    if globalStatus == 'Busy':
+        awayCall()
+    else:
+        busyCall()
 
-  return make_response(jsonify({'status': globalStatus}), 200)
+    return make_response(jsonify({'status': globalStatus}), 200)
+
 
 @app.route('/api/statusraw', methods=['GET'])
 def apiStatusRaw():
-        global globalStatus
-        return make_response(globalStatus, 200)
+    global globalStatus
+    return make_response(globalStatus, 200)
+
 
 @app.errorhandler(404)
 def not_found(error):
-	return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 def startupRainbow():
-	global blinkThread
-	blinkThread = threading.Thread(target=displayRainbow, args=(1, 0.1, 1))
-	blinkThread.do_run = True
-	blinkThread.start()
+    global blinkThread
+    blinkThread = threading.Thread(target=displayRainbow, args=(1, 0.1, 1))
+    blinkThread.do_run = True
+    blinkThread.start()
+
 
 button_a = Button(5)
 button_b = Button(6)
@@ -359,10 +393,10 @@ button_x = Button(16)
 button_y = Button(24)
 
 try:
-#    button_a.when_pressed = pressed
-#    button_b.when_pressed = pressed
-#    button_x.when_pressed = pressed
-#    button_y.when_pressed = pressed
+    #    button_a.when_pressed = pressed
+    #    button_b.when_pressed = pressed
+    #    button_x.when_pressed = pressed
+    #    button_y.when_pressed = pressed
 
     with app.app_context():
         button_a.when_pressed = apiOff
@@ -377,4 +411,4 @@ except KeyboardInterrupt:
     button_y.close()
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=False)
